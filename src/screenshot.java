db3c8acf52517613d4c70da3331724e39bec6dd1
dinesh4200 <Vdinesh@1234>
2019-04-01 19:59:59 +0530

    first attempt

---
+++ src/screenshot.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class screenshot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		 WebDriver driver=new ChromeDriver();
+		driver.get("https://www.udemy.com/");
+		
+
+	}
+
+}
